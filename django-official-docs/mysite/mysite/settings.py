7c17b6151ccde18a97ffcb416ab56da06b2eecd2
ceezous <ceezous@gmail.com>
2021-09-02 08:46:17 +0800

    finish part2

--- django-official-docs/mysite/mysite/settings.py
+++ django-official-docs/mysite/mysite/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'Asia/Shanghai'
 

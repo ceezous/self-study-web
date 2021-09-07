ead8bbc4c2bb8bb257a20f7b1869fdf81b59de74
ceezous <ceezous@gmail.com>
2021-09-07 08:43:01 +0800

    Finish part3.

--- django-official-docs/mysite/polls/urls.py
+++ django-official-docs/mysite/polls/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 
+# *** notes by crz at 2021-09-07 08:25 *** : Specify namespace.
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name="index"), # *** notes by crz at 2021-09-07 08:41 *** : name是为了让template中的对象逆向路由
     path('<int:question_id>/', views.detail, name="detail"),
     path('<int:question_id>/results/', views.results, name="results"),
     path('<int:question_id>/vote/', views.vote, name="vote"),
